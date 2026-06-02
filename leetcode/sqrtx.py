24cb458b5d0e86edeb3b36f880fe6d44cf8f0175
tanisha ⋆ ˚｡⋆౨ৎ˚ <142703435+tsthuti@users.noreply.github.com>
2026-06-02 17:49:45 -0400

    Add solution for sqrtx

---
+++ leetcode/sqrtx.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+            
+        left, right = 1, x // 2
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return right
